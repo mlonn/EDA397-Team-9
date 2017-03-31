a638249dfc15c09681abcbe7fa0845efc115c362
DebScap <debora.scappin@gmail.com>
2017-03-31 17:08:29 +0200

    GUI

--- app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/MainActivity.java
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/MainActivity.java
@@ -2,6 +2,7 @@ package se.chalmers.eda397.team9.cardsagainsthumanity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickNext(View view) {
+        //just for testing
+        setContentView(R.layout.activity_join_game);
+    }
+
+    public void clickJoin(View view) {
+        //just for testing
+        setContentView(R.layout.activity_player_list);
+    }
+
+    public void clickTableRule(View view) {
+        //just for testing
+        setContentView(R.layout.activity_create_rule);
     }
 }
