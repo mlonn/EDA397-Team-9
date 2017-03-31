9558f1e396249461db262e813b46f6f6a605583f
AxelEkdahl <axel_93_ekdahl@hotmail.com>
2017-03-31 09:41:37 +0200

    Created Player class

---
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/Player.java
@@ -0,0 +1,15 @@
+package se.chalmers.eda397.team9.cardsagainsthumanity;
+
+/**
+ * Created by axel_ on 2017-03-31.
+ */
+
+public class Player {
+
+    public String name = "";
+    public int currentScore = 0;
+    private String ip_adress = "";
+    public boolean isKing;
+
+
+}
