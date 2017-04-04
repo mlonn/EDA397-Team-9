f1f24d83223e8d2577f3d6f6f00f2cdea450e19a
Alex Tao <talex@student.chalmers.se>
2017-04-04 16:58:27 +0200

    Added P2P logic to CreateTableActivity

--- app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivity.java
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivity.java
@@ -1,31 +1,50 @@
 package se.chalmers.eda397.team9.cardsagainsthumanity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import se.chalmers.eda397.team9.cardsagainsthumanity.Classes.Player;
+import se.chalmers.eda397.team9.cardsagainsthumanity.Classes.Table;
+import se.chalmers.eda397.team9.cardsagainsthumanity.P2PClasses.WiFiBroadcastReceiver;
 
 
+public class CreateTableActivity extends AppCompatActivity {
 
-public class CreateTableActivity extends Activity {
 
+    private WifiP2pManager wifiManager;
+    private WifiP2pManager.Channel channel;
+    private WiFiBroadcastReceiver receiver;
+    private IntentFilter mIntentFilter;
+    private Map<String, Table> tables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tables = new HashMap<String, Table>();
         setContentView(R.layout.activity_createtable);
+        initP2p();
         Intent intent = getIntent();
         final Button createTableButton = (Button) findViewById(R.id.createTable_button);
         Button joinTableButton = (Button) findViewById(R.id.joinTable_button);
 
+        final EditText tableName = (EditText)findViewById(R.id.tablename);
 
         createTableButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Table table = new Table(tableName.getText().toString());
+                tables.put(tableName.getText().toString(), table);
                 Intent intent = new Intent(v.getContext(), CreateRuleActivity.class);
                 startActivity(intent);
             }
@@ -35,6 +54,36 @@ public class CreateTableActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), CreatePlayerListActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void initP2p(){
+
+        wifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = wifiManager.initialize(this, getMainLooper(), null);
+        receiver = new WiFiBroadcastReceiver(wifiManager, channel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        discoverPeers();
+
+    }
+
+    private void discoverPeers(){
+        wifiManager.discoverPeers(channel, new WifiP2pManager.ActionListener(){
+
+            @Override
+            public void onSuccess() {
+                System.out.println("Discovery successful");
+            }
+            @Override
+            public void onFailure(int reason) {
+                System.out.println("Discovery failed, " + reason );
             }
         });
     }
