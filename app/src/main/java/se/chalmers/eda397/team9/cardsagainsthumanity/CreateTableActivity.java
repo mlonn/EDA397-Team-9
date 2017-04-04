9dfe05c3a1f3ffad4dd86c2010717655c4f2114a
Mohannad <eng.moh205@gmail.com>
2017-04-04 15:56:06 +0200

    - GUI: Create PlayerList - CreateRuleActivity

--- app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivity.java
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,30 +20,22 @@ public class CreateTableActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createtable);
         Intent intent = getIntent();
-        final Button button = (Button) findViewById(R.id.createTable_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button createTableButton = (Button) findViewById(R.id.createTable_button);
+        Button joinTableButton = (Button) findViewById(R.id.joinTable_button);
+
+
+        createTableButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Save the submitted tablename
-                EditText usernameInput = (EditText) findViewById(R.id.tablename);
-                Player player = new Player(usernameInput.toString());
-                SharedPreferences.Editor editor = getSharedPreferences("tableNameFile", MODE_PRIVATE).edit();
-                editor.putString("name", usernameInput.getText().toString());
-                editor.commit();
-                if(v.getId() == R.id.createTable_button)
-                goToRule(v);
-                else if(v.getId() == R.id.joinTable_button)
-                    goToPlayerList(v);
+                Intent intent = new Intent(v.getContext(), CreateRuleActivity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    public void goToRule(View view) {
-        Intent intent = new Intent(this, CreateRuleActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToPlayerList(View view) {
-        Intent intent = new Intent(this, CreatePlayerList.class);
-        startActivity(intent);
+        joinTableButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), CreatePlayerListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
