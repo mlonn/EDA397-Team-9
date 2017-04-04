cff0e11eebb87a9a46d055d8a17a190462f990e7
AxelEkdahl <axel_93_ekdahl@hotmail.com>
2017-04-04 17:24:23 +0200

    App now remeber if already typed in a username and redirect to CreateTableActivity

--- app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/IndexActivity.java
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/IndexActivity.java
@@ -1,14 +1,17 @@
 package se.chalmers.eda397.team9.cardsagainsthumanity;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.File;
 
 import se.chalmers.eda397.team9.cardsagainsthumanity.Classes.Player;
 
@@ -22,19 +25,50 @@ public class IndexActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        final Context context = this;
+        final Player player = new Player();
+
+        //Check if the application has a Shared Preferences file containing a username already
+        if(fileExists()){
+            player.username = player.getUsername(context);
+            Intent intent = new Intent(this, CreateTableActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         final Button button = (Button) findViewById(R.id.btn_submitUsername);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Save the submitted username
                 EditText usernameInput = (EditText) findViewById(R.id.txt_username);
-                Player player = new Player(usernameInput.toString());
-                SharedPreferences.Editor editor = getSharedPreferences("usernameFile", MODE_PRIVATE).edit();
-                editor.putString("name", usernameInput.getText().toString());
-                editor.commit();
+                createUsernameFile(usernameInput.toString());
+                player.username = player.getUsername(context);
+
                 gotoCreateTable(v);
             }
         });
+    }
 
+    private void createUsernameFile(String username2Save){
+        SharedPreferences.Editor editor = getSharedPreferences("usernameFile", MODE_PRIVATE).edit();
+        editor.putString("name", username2Save);
+        editor.commit();
+    }
+
+    private boolean fileExists(){
+
+        String sharePrefName = "usernameFile.xml";
+        File f = new File("/data/data/se.chalmers.eda397.team9.cardsagainsthumanity/shared_prefs/"+ sharePrefName);
+        if (f.exists())
+        {
+            Log.d("TAG", "SharedPreferences Name_of_your_preference : exist");
+            return true;
+        }
+
+        else
+        {
+            Log.d("TAG", "Setup default preferences");
+            return false;
+        }
     }
 
     private void gotoCreateTable(View view) {
