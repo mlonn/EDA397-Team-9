0e1a6ab2222d5538c89c40ae9e9062778ec67aea
Mohannad <eng.moh205@gmail.com>
2017-04-04 12:14:38 +0200

    Last Updates

---
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreatePlayerList.java
@@ -0,0 +1,13 @@
+package se.chalmers.eda397.team9.cardsagainsthumanity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePlayerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_player_list);
+    }
+}
