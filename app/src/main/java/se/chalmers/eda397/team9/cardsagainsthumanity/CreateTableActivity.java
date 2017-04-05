58bf91aa776aa06d8e38bcb53595f7d8f26346d7
Mikael Lönn <mikael.lonn@outlook.com>
2017-04-05 22:52:33 +0200

    Devices connect now

--- app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivity.java
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,7 @@ public class CreateTableActivity extends AppCompatActivity {
     private IntentFilter mIntentFilter;
     private Map<String, Table> tables;
     private Intent createTableIntent;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +39,37 @@ public class CreateTableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_createtable);
         initP2p();
         Intent intent = getIntent();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_to_refresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                initP2p();
+                swipeRefreshLayout.setRefreshing(false);
+
+            }
+        });
         final Button createTableButton = (Button) findViewById(R.id.createTable_button);
         Button joinTableButton = (Button) findViewById(R.id.joinTable_button);
 
-        final EditText tableName = (EditText)findViewById(R.id.tablename);
+        final EditText tableName = (EditText) findViewById(R.id.tablename);
 
         createTableButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Table table = new Table(tableName.getText().toString());
                 tables.put(tableName.getText().toString(), table);
                 Intent intent = new Intent(v.getContext(), CreateRuleActivity.class);
-                startActivity(intent);
+
 
                 //Creates an intent with table information
                 createTableIntent = new Intent("WIFI_NEW_TABLE_INFO");
                 createTableIntent.addCategory("CARDS_AGAINST_HUMANITY");
                 createTableIntent.putExtra("TABLE_NAME", table.getName());
-                createTableIntent.putExtra("HOST_NAME", table.getHost());
-                createTableIntent.putExtra("TABLE_SIZE", table.getSize());
+                // createTableIntent.putExtra("HOST_NAME", table.getHost());
+                // createTableIntent.putExtra("TABLE_SIZE", table.getSize());
                 //Broadcasts this intent
                 sendBroadcast(createTableIntent);
+
             }
         });
 
@@ -68,7 +81,21 @@ public class CreateTableActivity extends AppCompatActivity {
         });
     }
 
-    private void initP2p(){
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, mIntentFilter);
+    }
+
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    private void initP2p() {
 
         wifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         channel = wifiManager.initialize(this, getMainLooper(), null);
@@ -84,21 +111,21 @@ public class CreateTableActivity extends AppCompatActivity {
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
-        discoverPeers();
-
-    }
-
-    private void discoverPeers(){
-        wifiManager.discoverPeers(channel, new WifiP2pManager.ActionListener(){
+        wifiManager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
 
             @Override
             public void onSuccess() {
                 System.out.println("Discovery successful");
             }
+
             @Override
             public void onFailure(int reason) {
-                System.out.println("Discovery failed, " + reason );
+                System.out.println("Discovery failed, " + reason);
             }
         });
+
     }
+
+    
+
 }
