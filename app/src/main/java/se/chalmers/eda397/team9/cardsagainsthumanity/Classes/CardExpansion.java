a7068c120f6f0c0259f68661012a0826980038ee
Mikael Lönn <mikael.lonn@outlook.com>
2017-04-21 11:55:31 +0200

    Creates game and gives players cards

--- app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/Classes/CardExpansion.java
+++ app/src/main/java/se/chalmers/eda397/team9/cardsagainsthumanity/Classes/CardExpansion.java
@@ -3,7 +3,6 @@ package se.chalmers.eda397.team9.cardsagainsthumanity.Classes;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +11,7 @@ import java.util.List;
  */
 
 public class CardExpansion {
+
     private List<BlackCard> blackCardList;
     private List<WhiteCard> whiteCardList;
     private String name;
@@ -32,7 +32,7 @@ public class CardExpansion {
                 blackCardList.add(new BlackCard(b.getString("text"), b.getInt("pick")));
             }
             for(int i = 0; i < white.length(); i++) {
-                whiteCardList.add(new WhiteCard("hej"));
+                whiteCardList.add(new WhiteCard(whiteCards.getString(Integer.parseInt(white.getString(i)))));
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -42,19 +42,26 @@ public class CardExpansion {
         return name;
     }
 
-
-    public int getNoBlackCards() {
-        return blackCardList.size();
-    }
-    public int getNoWhiteCards() {
-        return whiteCardList.size();
+    public List<BlackCard> getBlackCards() {
+        return blackCardList;
     }
 
-    public boolean getSelection(){
-        return selected;
+    public void setBlackCardList(List<BlackCard> blackCardList) {
+        this.blackCardList = blackCardList;
+    }
+
+    public List<WhiteCard> getWhiteCards() {
+        return whiteCardList;
+    }
+
+    public void setWhiteCardList(List<WhiteCard> whiteCardList) {
+        this.whiteCardList = whiteCardList;
     }
 
     public void setSelection(boolean s) {
         selected = s;
+    }
+    public boolean isSelected(){
+        return selected;
     }
 }
