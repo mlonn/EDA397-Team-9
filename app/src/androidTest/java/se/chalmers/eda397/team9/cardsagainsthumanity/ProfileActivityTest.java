eebcab3a2375e1ef47ec3d8210c7f2023e1a3974
Mohannad <eng.moh205@gmail.com>
2017-05-12 23:16:36 +0200

    Add ProfileActivityTest

---
+++ app/src/androidTest/java/se/chalmers/eda397/team9/cardsagainsthumanity/ProfileActivityTest.java
@@ -0,0 +1,38 @@
+package se.chalmers.eda397.team9.cardsagainsthumanity;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.TextView;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by TOSHIBA on 12/05/2017.
+ */
+public class ProfileActivityTest {
+
+    @Rule
+    public ActivityTestRule<ProfileActivity> mActivityTestRule = new ActivityTestRule<ProfileActivity>(ProfileActivity.class);
+    private ProfileActivity mActivity = null;
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testProfileName(){
+        TextView textView = (TextView) mActivity.findViewById(R.id.textView11);
+        assertNotNull(textView);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
