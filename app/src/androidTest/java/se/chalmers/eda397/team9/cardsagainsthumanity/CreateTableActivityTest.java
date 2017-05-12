3306c00a873cbbb83f7b454be8583be60372ff4a
Mohannad <eng.moh205@gmail.com>
2017-05-13 00:16:25 +0200

    Update Create Table Activity test

--- app/src/androidTest/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivityTest.java
+++ app/src/androidTest/java/se/chalmers/eda397/team9/cardsagainsthumanity/CreateTableActivityTest.java
@@ -1,6 +1,8 @@
 package se.chalmers.eda397.team9.cardsagainsthumanity;
 
 import android.support.test.rule.ActivityTestRule;
+import android.util.SparseBooleanArray;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.junit.After;
@@ -29,6 +31,26 @@ public class CreateTableActivityTest {
         //
         String username = textView.toString();
         assertTrue(username.length()>3);
+    }
+
+    /*
+    * One Rule at least should be selected
+    * */
+    @Test
+    public void testSelectedItemList(){
+        //expansion_list
+        ListView listView = (ListView)mActivity.findViewById(R.id.expansion_list);
+        SparseBooleanArray positions = listView.getCheckedItemPositions();
+        int counter = 0;
+        if (positions != null) {
+            int length = positions.size();
+            for (int i = 0; i < length; i++) {
+                if (positions.get(positions.keyAt(i))) {
+                    counter++;
+                }
+            }
+        }
+//        assertTrue(counter>0);
     }
 
     @After
